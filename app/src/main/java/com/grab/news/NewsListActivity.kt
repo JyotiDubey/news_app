ecb73b11f3d0df297ae9e5052a006e721f19213b
Jyoti Dubey <jyoti0801@gmail.com>
2019-03-09 09:10:45 +0530

    Add required dependencies

---
+++ app/src/main/java/com/grab/news/NewsListActivity.kt
@@ -0,0 +1,12 @@
+package com.grab.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_list)
+    }
+}
