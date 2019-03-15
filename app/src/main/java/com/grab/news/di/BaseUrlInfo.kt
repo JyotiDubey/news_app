94fb3b6c279578bd8a4d876193617492f366f636
Jyoti Dubey <jyoti0801@gmail.com>
2019-03-15 12:44:02 +0530

    Add Retrofit Module

---
+++ app/src/main/java/com/grab/news/di/BaseUrlInfo.kt
@@ -0,0 +1,12 @@
+package com.grab.news.di
+
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import javax.inject.Qualifier
+
+/**
+ * Created by jyotidubey on 2019-03-15.
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+annotation class BaseUrlInfo
---
+++ app/src/main/java/com/grab/news/di/module/RetrofitModule.kt
@@ -0,0 +1,50 @@
+package com.grab.news.di.module
+
+import android.content.Context
+import com.grab.news.BASE_URL
+import com.grab.news.data.remote.retrofit.NewsService
+import com.grab.news.di.BaseUrlInfo
+import dagger.Module
+import dagger.Provides
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Created by jyotidubey on 2019-03-15.
+ */
+@Module
+class RetrofitModule(val context: Context) {
+
+    @Provides
+    @Singleton
+    internal fun provideApiInterface(retrofit: Retrofit): NewsService {
+        return retrofit.create<NewsService>(NewsService::class.java)
+    }
+
+    @Provides
+    @BaseUrlInfo
+    internal fun provideBaseUrl() = BASE_URL
+
+    @Provides
+    @Singleton
+    internal fun provideRetrofit(okHttpClient: OkHttpClient, @BaseUrlInfo url: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    internal fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+}
